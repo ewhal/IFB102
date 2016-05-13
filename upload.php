7152eecfa6f364e734d1c771d8dacb204d80f9d9
Eliot Whalan <ewhal@pantsu.cat>
2016-05-13 20:32:56 +1000

    Fix up upload.php

--- upload.php
+++ upload.php
@@ -2,16 +2,16 @@
 $uploads_dir = 'uploads/';
 
 foreach ($_FILES["files"]["error"] as $key => $error) {
-	if ($error == UPLOAD_ERR_OK) {
-		$tmp_name = $_FILES["files"]["tmp_name"][$key];
-		$name = $_FILES["files"]["name"][$key];
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["files"]["tmp_name"][$key];
+        $name = $_FILES["files"]["name"][$key];
 		$upload = "$uploads_dir$name";
-
-		move_uploaded_file($tmp_name, "$upload");
+        move_uploaded_file($tmp_name, "$upload");
 		chmod($upload, 0777);
-
-		echo "<a href=\"$upload\">$upload</a><br>";
-	}
+		exec("mpc add $name");
+    }
 }
 exec("mpc update");
+echo "<a href=\"$_SERVER['SERVER_ADDR']:8000\/mpd\">IceCast Steam</a>"; 
 ?>
+
