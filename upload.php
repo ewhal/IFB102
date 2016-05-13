16edebd167b4bc906f8c2e85b0574a29f6b11a6c
Eliot Whalan <ewhal@pantsu.cat>
2016-05-13 18:19:27 +1000

    Rework upload.php

--- upload.php
+++ upload.php
@@ -1,12 +1,16 @@
 <?php
+$uploads_dir = 'uploads/';
 
-if(count($_FILES['files'])) {
-	foreach ($_FILES['files'] as $file) {
-	    
-		//do your upload stuff here
-		echo $file;
-		
+foreach ($_FILES["files"]["error"] as $key => $error) {
+	if ($error == UPLOAD_ERR_OK) {
+		$tmp_name = $_FILES["files"]["tmp_name"][$key];
+		$name = $_FILES["files"]["name"][$key];
+		$upload = "$uploads_dir$name";
+
+		move_uploaded_file($tmp_name, "$upload");
+		chmod($upload, 0777);
+
+		echo "<a href=\"$upload\">$upload</a><br>";
 	}
 }
-
 ?>
