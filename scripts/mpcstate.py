dfe21ec34861b2fef0a61e7d55428efca47b451c
Eliot Whalan <ewhal@pantsu.cat>
2016-05-15 23:23:14 +1000

    Fix up mpcstate

--- scripts/mpcstate.py
+++ scripts/mpcstate.py
@@ -15,6 +15,7 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 while True:
@@ -24,37 +25,34 @@ while True:
     next_state = GPIO.input(12)
     volume_down_state = GPIO.input(24)
     volume_up_state = GPIO.input(4)
+    ip = GPIO.input(23)
 
     if prev_state == False:
-        print "button 16"
+	print "16"
         system("mpc prev")
-	system("python /root/lcd.py")
-        sleep(0.2)
+    	sleep(0.2)
     if next_state == False:
-        print "button 12"
+	print "12"
         system("mpc next")
-	system("python /root/lcd.py")
-        sleep(0.2)
+    	sleep(0.2)
     if volume_down_state == False:
-        print "button 24"
+	print "24"
         system("mpc volume -10")
-        sleep(0.2)
     if volume_up_state == False:
-        print "button 4"
+	print "4"
         system("mpc volume +10")
-        sleep(0.2)
+    if ip == False:
+	print "23"
+	system("python /root/ip.py")
+    	sleep(0.2)
+
 
 
     if stop_state == False:
-    	print "button 21"
-    	state = False
+	print "21"
     	system("mpc stop")
-    	sleep(0.2)
 
     if start_state == False:
-    	print "button 18"
-    	start = True
+	print "18"
 	system("mpc play")
-	system("python /root/lcd.py")
-    	sleep(0.2)
 
